e742b0e2fe4b3d85ed9d041555be159c1e7efea6
Legacy-6-7 <reservedplanet1233@gmail.com>
2026-09-02 09:21:32 +0530

    Fix Render host configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,11 +11,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    "portfolio-g1se.onrender.com",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.staticfiles",
